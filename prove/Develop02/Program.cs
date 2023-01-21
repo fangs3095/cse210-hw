e2fd75cdbeed2ae71ea8013ea3433196c604047e
Barxton Keyes <umberon3095@gmail.com>
2023-01-21 09:16:29 -0700

    hi team
    Co-authored-by: dicmaryzambrano <DicmaryZambrano@users.noreply.github.com>

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine("What would you like to do?");
+        Console.WriteLine("1-Write");
+        Console.WriteLine("2-Display");
+        Console.WriteLine("3-Save");
+        Console.WriteLine("4-Load");
+        Console.WriteLine("5-Quit");
+
+        Console.ReadLine()
+
     }
 }
+
